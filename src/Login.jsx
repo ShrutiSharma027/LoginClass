f779120fa38b55b5adec4090236e974c2740678d
Shruti WebGit <27shrutisharma.06@gmail.com>
2024-05-09 19:44:17 +0530

    Login and Signup commited successfully

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { Signup } from "./Signup";
+import log from "./assets/images/log.jpg"
+import google from "./assets/images/google.png"
 
 export const Login=()=> {
     return (
@@ -10,13 +12,15 @@ export const Login=()=> {
                 <div className="row d-flex justify-content-center shadow align-items-center bg-white box-area ">
                     {/* <!---------------------- Left Box -------------------------------> */}
                     <div className="col-lg-6 col-md-6 col-sm-4 ">
-                        <div className="image"></div>
+                        <div className="image">
+                        <img src={log} className="image" alt="Login Image"/>
+                        </div>
                     </div>
                     {/* <!---------------------- Right Box -------------------------------> */}
                     <div className="col-md-6 ">
                         <div className="row align-items-center">
                             <div className="header text mb-4 ">
-                                <h4 className="d-flex justify-content-center align-items-center">Login</h4>
+                                <h4 className="d-flex justify-content-center align-items-center mt-4">Login</h4>
                                 <h4 className="d-flex justify-content-center align-items-center">We are happy to have you back.
                                 </h4>
                             </div>
@@ -36,7 +40,7 @@ export const Login=()=> {
                                     <label htmlFor="formCheck" className="form-check-label text"><small> Remember me
                                     </small></label>
                                 </div>
-                                <div className="forgot">
+                                <div className="forgot margin-right">
                                     <small><a href="#">Forgot password</a></small>
                                     <div>
                                     </div>
@@ -46,11 +50,13 @@ export const Login=()=> {
                                 <button className="btn btn-lg btn-primary w-50 fs-10">Login</button>
                             </div>
                             <div className="input-group mb-3">
-                                <button className="btn btn-lg btn-light w-100 fs-10 "><div className="google me-2" alt="Google image"></div>
+                                <button className="btn btn-lg btn-light w-100 fs-10 ">
+                                    {/* <div className="google" alt="Google image"></div> */}
+                                    <img src={google} className="google" alt="Google image"/>
                                 <small>Sign in with Google</small></button>
                             </div>
-                            <div className="row">
-                                <small>Don't have an account? <NavLink to={Signup}>Sign Up</NavLink></small>
+                            <div className="row mb-3">
+                                <small>Don't have an account? <NavLink to="/signup">Sign Up</NavLink></small>
                             </div>
                         </div>
                     </div>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,73 +1,75 @@
 import { NavLink } from "react-router-dom";
 import { Login } from "./Login";
+import log from "./assets/images/log.jpg"
+import google from "./assets/images/google.png"
 
-export const Signup=()=> {
-      return (
-    <>
-    {/* <!---------------------- Main Container -------------------------> */}
-    <div className="container-fluid margin d-flex justify-content-center align-items-center ">
-        {/* <!---------------------- Login Container ------------------------> */}
-        <div className="row d-flex justify-content-center shadow align-items-center bg-white box-area mt-0">
-            {/* <!---------------------- Left Box -------------------------------> */}
-            <div className="col-md-6 col-md-6 col-sm-4 ">
-            <div className="image"></div>
-            </div>
-            {/* <!---------------------- Right Box -------------------------------> */}
-            <div className="col-md-6 d-flex justify-content-center align-items-center bg-white right-box">
-                <div className="row align-items-center">
-                    <div className="header text mb-4 ">
-                        <h4 className="d-flex justify-content-center align-items-center">Sign Up</h4>
-                        <h4 className="d-flex justify-content-center align-items-center">Register here to sign up on our
-                            website</h4>
-                    </div>
-                    <div className="input-group mb-4">
-                        <label>Enter Your Name<input type="email"
-                                className="form-control form-control-lg bg-light round required"
-                                placeholder="Username"/></label>
-                    </div>
-                    <div className="input-group mb-4">
-                        <label>Enter Your Email<input type="password"
-                                className="form-control form-control-lg bg-light round required"
-                                placeholder="Email"/></label>
-                    </div>
-                    <div className="input-group mb-4">
-                        <label>Enter Password<input type="password"
-                                className="form-control form-control-lg bg-light round required"
-                                placeholder="Password"/></label>
-                    </div>
-                    <div className="input-group mb-4">
-                        <label>Enter Confirm Password<input type="password"
-                                className="form-control form-control-lg bg-light round required"
-                                placeholder="Password"/></label>
-                    </div>
-                    <div className="input-group mb-5 d-flex justify-content-between">
-                        <div className="form-check">
-                            <input type="checkbox" className="form-check-input" id="formCheck"/>
-                            <label htmlFor="formCheck" className="form-check-label text"><small> Remember me
-                                </small></label>
+export const Signup = () => {
+    return (
+        <>
+            {/* <!---------------------- Main Container -------------------------> */}
+            <div className="container-fluid margin d-flex justify-content-center align-items-center ">
+                {/* <!---------------------- Login Container ------------------------> */}
+                <div className="row d-flex justify-content-center shadow align-items-center bg-white box-area mt-0">
+                    {/* <!---------------------- Left Box -------------------------------> */}
+                    <div className="col-md-6 col-md-6 col-sm-4 ">
+                        <div className="image">
+                            <img src={log} className="image" alt="Login Image" />
                         </div>
-                        <div className="forgot">
-                            <small><NavLink href="#">Forgot password</NavLink></small>
-                            <div>
+                    </div>
+                    {/* <!---------------------- Right Box -------------------------------> */}
+                    <div className="col-md-6 d-flex justify-content-center align-items-center bg-white right-box">
+                        <div className="row align-items-center">
+                            <div className="header text mb-4 ">
+                                <h4 className="d-flex justify-content-center align-items-center mt-4">Sign Up</h4>
+                                <h4 className="d-flex justify-content-center align-items-center">Register here to sign up on our
+                                    website</h4>
+                            </div>
+                            <div className="input-group mb-4">
+                                <label>Enter Your Name<input type="email"
+                                    className="form-control form-control-lg bg-light round required"
+                                    placeholder="Username" /></label>
+                            </div>
+                            <div className="input-group mb-4">
+                                <label>Enter Your Email<input type="password"
+                                    className="form-control form-control-lg bg-light round required"
+                                    placeholder="Email" /></label>
+                            </div>
+                            <div className="input-group mb-4">
+                                <label>Enter Password<input type="password"
+                                    className="form-control form-control-lg bg-light round required"
+                                    placeholder="Password" /></label>
+                            </div>
+                            <div className="input-group mb-4">
+                                <label>Enter Confirm Password<input type="password"
+                                    className="form-control form-control-lg bg-light round required"
+                                    placeholder="Password" /></label>
+                            </div>
+                            <div className="input-group mb-5 d-flex justify-content-between">
+                                <div className="form-check">
+                                    <input type="checkbox" className="form-check-input" id="formCheck" />
+                                    <label htmlFor="formCheck" className="form-check-label text"><small> Remember me
+                                    </small></label>
+                                </div>
+                                <div className="forgot margin-right">
+                                    <small><NavLink href="#">Forgot password</NavLink></small>
+                                </div>
+                            </div>
+                            <div className="col-md-12 justify-content-center input-group mb-3">
+                                <button className="btn btn-lg btn-primary w-50 fs-10">Sign Up</button>
+                            </div>
+                            <div className="input-group mb-3">
+                                <button className="btn btn-lg btn-light w-100 fs-10 "><img src={google}
+                                    className="google" alt="Google image" /><small>Sign in with Google</small></button>
+                            </div>
+                            <div className="row mb-3">
+                                <small>Already have an account? <NavLink to="/login">Login</NavLink></small>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-12 justify-content-center input-group mb-3">
-                        <button className="btn btn-lg btn-primary w-50 fs-10">Sign Up</button>
-                    </div>
-                    <div className="input-group mb-3">
-                        <button className="btn btn-lg btn-light w-100 fs-10 "><img src="./images/google.png"
-                                className="google me-2" alt="Google image"/><small>Sign in with Google</small></button>
-                    </div>
-                    <div className="row">
-                        <small>Already have an account? <NavLink to={Login}>Login</NavLink></small>
-                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 // export default Signup
